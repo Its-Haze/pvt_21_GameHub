3535b05190d0d215e23a7a9cd39e4b00cd4887f3
francesca <francescapanescu@yahoo.com>
2021-11-15 16:46:10 +0100

    Placed test_surface bottom right

--- Tetris.py
+++ Tetris.py
@@ -5,11 +5,17 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100, 200))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+
     screen.fill('WHITE')
+    screen.blit(test_surface, (700, 200))
     pygame.display.update()
+    clock.tick(60)
+
