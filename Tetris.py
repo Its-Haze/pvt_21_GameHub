cfcbbbaa74510a6ddc0389712d388898aea0a421
Axel <axel.lindstrom@effectsoft.se>
2021-11-16 09:06:37 +0100

    Added text surface

--- Tetris.py
+++ Tetris.py
@@ -1,21 +1,24 @@
 import pygame
 from sys import exit
 
-
-
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100, 200))
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+text_surface = test_font.render('Tetris', False, 'Black')
+text_rectangle = text_surface.get_rect(midtop=(400, 50))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-
-    screen.fill('WHITE')
-    screen.blit(test_surface, (700, 200))
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, text_rectangle)
     pygame.display.update()
     clock.tick(60)
 
