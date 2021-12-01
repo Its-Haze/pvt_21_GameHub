fb14a3d5a5f798c3649e1282ca09a658137769c0
Erik <jobb.avakian@gmail.com>
2021-12-01 09:48:24 +0100

    fixade färger på texterna i paint och ändrade lite image.load strängar

--- instructions_screen.py
+++ instructions_screen.py
@@ -15,17 +15,19 @@ def show_intro_screen(game_name):
 
     clock = pygame.time.Clock()
     running = True
-    
 
     pygame.font.init()  # Behövs för att initiera fonts
 
-    play_surface = pygame.image.load('menu_buttons/start_button_text.png').convert_alpha()  # surface - Sky.png
+    play_surface = pygame.image.load(
+        'Instructions_folder/menu_buttons/start_button_text.png').convert_alpha()  # surface - Sky.png
     play_rect = play_surface.get_rect(midtop=(400, 120))
-    
-    guide_surface = pygame.image.load("menu_buttons/guide_button_text.png").convert_alpha()
+
+    guide_surface = pygame.image.load(
+        "Instructions_folder/menu_buttons/guide_button_text.png").convert_alpha()
     guide_rect = guide_surface.get_rect(midtop=(400, 260))
-    
-    back_surface = pygame.image.load("menu_buttons/back_button_text.png").convert_alpha()
+
+    back_surface = pygame.image.load(
+        "Instructions_folder/menu_buttons/back_button_text.png").convert_alpha()
     back_rect = back_surface.get_rect(midtop=(400, 400))
 
     bg_sound_hub = pygame.mixer.Sound('audio/hub.mp3')
@@ -34,7 +36,7 @@ def show_intro_screen(game_name):
 
     while running:
         from hubtest1 import start_game_hub
-        # When user click "runner", play "runner"
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -56,40 +58,39 @@ def show_intro_screen(game_name):
                     print("klickade på back knappen")
                     bg_sound_hub.stop()
                     start_game_hub()
-     
+
         if running:
             screen.fill("black")
-            
+
             # Rotation
             # True
             if surf_rotation_bool:
                 surf_rotation -= 1
                 surf_scale -= 0.01
-                
+
             if surf_rotation == -20:
                 surf_rotation_bool = False
             # False
             if not surf_rotation_bool:
                 surf_rotation += 1
                 surf_scale += 0.01
-                
+
             if surf_rotation == 20:
                 surf_rotation_bool = True
 
+            font = pygame.font.Font("Instructions_folder/font/FFFFORWA.TTF", 10)
+            title_text = font.render(f"{game_name}", False, "white")
+            title_rotozoom_text = pygame.transform.rotozoom(
+                title_text, surf_rotation, surf_scale)
+            title_rect = title_rotozoom_text.get_rect(midtop=(400, 20))
 
-            
-            font = pygame.font.SysFont('Comic Sans MS', 20)
-            title_text = font.render(game_name, False, "White")
-            title_rotozoom_text = pygame.transform.rotozoom(title_text, surf_rotation, surf_scale)
-            title_rect = title_rotozoom_text.get_rect(midtop=(400, 10))
-            
             screen.blit(title_rotozoom_text, title_rect)
             screen.blit(play_surface, play_rect)
             screen.blit(guide_surface, guide_rect)
             screen.blit(back_surface, back_rect)
-            
+
             # Change screen to the current game
-            
+
             pygame.display.update()
             clock.tick(60)
 
